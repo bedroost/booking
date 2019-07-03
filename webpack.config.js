a8d9472bd4a5355144138216d47b25b8e85556cc
esme <23175119+esme@users.noreply.github.com>
2019-07-03 16:05:57 -0700

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'client', 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(js|jsx)?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
+};
