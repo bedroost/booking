29e3177100caee5615dab2ce5ef5dca657e74973
esme <esme@github.com>
2019-07-04 22:24:05 -0700

    set sequelize schema fields to camelcase

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE bookings (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   reviews INT DEFAULT 0,
   views INT DEFAULT 0,
-  nightly_price INT,
+  price INT NOT NULL,
   cleaning_fee INT DEFAULT 0,
   service_fee INT DEFAULT 0,
   occupancy_fee_taxes INT DEFAULT 0,
@@ -14,8 +14,8 @@ CREATE TABLE bookings (
   children_guests INT DEFAULT 0,
   max_guests INT DEFAULT 5,
   max_months INT DEFAULT 3,
-  `createdAt` DATE NOT NULL,
-  `updatedAt` DATE NOT NULL
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME NOT NULL,
 );
 
 DROP TABLE IF EXISTS booking_dates;
@@ -25,6 +25,7 @@ CREATE TABLE booking_dates (
   booking_id INT NOT NULL,
   checkin_date DATE NOT NULL,
   checkout_date DATE NOT NULL,
-  `createdAt` DATE NOT NULL,
-  `updatedAt` DATE NOT NULL
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME NOT NULL,
+  FOREIGN KEY (booking_id) REFERENCES bookings(id)
 );
