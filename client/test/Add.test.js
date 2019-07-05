72e4350260b4fff93e57c53f4ae2a79f360471e4
esme <esme@github.com>
2019-07-05 12:03:49 -0700

    added testing for seed script

--- client/test/Add.test.js
+++ client/test/Add.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Add from '../src/App';
 
-describe('<Comment /> rendering', () => {
+xdescribe('<Comment /> rendering', () => {
   it('should render one <h1>', () => {
     const wrapper = shallow(<Add />);
     expect(wrapper.children('h1')).toHaveLength(0);
--- server/db/seed.js
+++ server/db/seed.js
@@ -6,26 +6,31 @@ const db = require('../db');
 const randomNumber = () => faker.random.number();
 const randomDate = () => faker.date.future();
 
-for (let i = 0; i < 100; i += 1) {
-  db.Booking.create({
-    reviews: randomNumber(),
-    views: randomNumber(),
-    price: randomNumber(),
-  })
-    .then((booking) => {
-      console.log(chalk.green((`created booking ${booking.id} successfully`)));
-    });
+module.exports.generateData = () => {
+  db.Booking.drop();
+  db.BookingDate.drop();
 
-  const checkinDate = randomDate();
-  const checkoutDate = moment(checkinDate).add(Math.floor(Math.random() * 10) + 1, 'day');
-  console.log(chalk.blue(checkinDate, checkoutDate));
+  for (let i = 0; i < 100; i += 1) {
+    db.Booking.create({
+      reviews: randomNumber(),
+      views: randomNumber(),
+      price: randomNumber(),
+    })
+      .then((booking) => {
+        console.log(chalk.green((`created booking ${booking.id} successfully`)));
+      });
 
-  db.BookingDate.create({
-    bookingId: i + 1,
-    checkinDate,
-    checkoutDate,
-  })
-    .then((bookingDate) => {
-      console.log(chalk.green((`created booking_date ${bookingDate.id} successfully`)));
-    });
-}
+    const checkinDate = randomDate();
+    const checkoutDate = moment(checkinDate).add(Math.floor(Math.random() * 10) + 1, 'day');
+    console.log(chalk.blue(checkinDate, checkoutDate));
+
+    db.BookingDate.create({
+      bookingId: i + 1,
+      checkinDate,
+      checkoutDate,
+    })
+      .then((bookingDate) => {
+        console.log(chalk.green((`created booking_date ${bookingDate.id} successfully`)));
+      });
+  }
+};
