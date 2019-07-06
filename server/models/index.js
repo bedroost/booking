ae519ade99015773a2a482ecaf4d86761305a75d
esme <esme@github.com>
2019-07-05 21:51:08 -0700

    added last available date

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,12 @@
 /* eslint-disable arrow-body-style */
 const db = require('../db');
 
-// module.exports.getListingInfo = (listingId) => {
-//   return db.Listing.findByPk(listingId);
-// };
+module.exports.getListingInfo = (listingId) => {
+  return db.Listing.findByPk(listingId);
+};
 
-// module.exports.getBookedDates = (listingId) => {
-//   return db.Booking.findAll({
-//     where: { listingId },
-//   });
-// };
+module.exports.getBookedDates = (listingId) => {
+  return db.Booking.findAll({
+    where: { listingId },
+  });
+};
