54607d7f891e4a93ada9851662bcfedce98cf1a0
esme <esme@github.com>
2019-07-06 15:22:24 -0700

    removed test

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,12 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 
-class Add extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Add Function</h1>
-      </div>
-    );
-  }
-}
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <Add />
-        </header>
+        <h1>hi</h1>
       </div>
     );
   }
