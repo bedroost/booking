df1a3cba32396089b8c6e834547cd383a63b2115
esme <23175119+esme@users.noreply.github.com>
2019-07-09 01:24:43 -0700

    reset checkin day when click on next month

--- client/src/reducers/calendar.js
+++ client/src/reducers/calendar.js
@@ -6,9 +6,9 @@ const initialState = {
 const calendarReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'NEXT_MONTH':
-      return { ...state, addMonth: state.addMonth + 1 };
+      return { ...state, addMonth: state.addMonth + 1, checkinDay: null };
     case 'LAST_MONTH':
-      return { ...state, addMonth: state.addMonth - 1 };
+      return { ...state, addMonth: state.addMonth - 1, checkinDay: null };
     case 'GET_DAY':
       if (!state.day) {
         return { ...state, checkinDay: action.payload };
