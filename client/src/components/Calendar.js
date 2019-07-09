691e3dbc890e0596a98f4bcddbf540efe6d5c798
esme <esme@github.com>
2019-07-08 22:39:17 -0700

    implement state for calendar days

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -4,43 +4,43 @@ import moment from 'moment';
 import styles from '../../styles/calendar.module.css';
 
 const Calendar = (props) => {
-  const firstDay = moment().date(1).day();
-  const lastDay = moment().daysInMonth();
+  const { addMonth, nextMonth, lastMonth, getDay } = props;
+
+  const firstDayofTheWeek = moment().add(addMonth, 'M').date(1).day();
+  const lastDay = moment().add(addMonth, 'M').daysInMonth();
   const month = [[], [], [], [], []];
   for (let i = 0; i < 7; i += 1) {
     for (let j = 0; j < 5; j += 1) {
-      if (i < firstDay && j === 0) {
+      if (i < firstDayofTheWeek && j === 0) {
         month[j].push(null);
       } else if (j * 7 + i > lastDay) {
         month[j].push(null);
       } else {
-        month[j].push(j * 7 + i);
+        month[j].push(j * 7 + i - firstDayofTheWeek + 1);
       }
     }
   }
-
-  const { addMonth, nextMonth, lastMonth } = props;
 
   return (
     <div className="CalendarModal">
       <div className="Calendar">
         <div className="CalendarMonth">
-          <div
+          <button
             className="CalendarMonthBackward"
-            role="button"
+            type="button"
             onClick={lastMonth}>
             <svg viewBox="0 0 1000 1000">
               <path d="M 336 275 L 126 485 h 806 c 13 0 23 10 23 23 s -10 23 -23 23 H 126 l 210 210 c 11 11 11 21 0 32 c -5 5 -10 7 -16 7 s -11 -2 -16 -7 L 55 524 c -11 -11 -11 -21 0 -32 l 249 -249 c 21 -22 53 10 32 32 Z" />
             </svg>
-          </div>
-          <div
+          </button>
+          <button
             className="CalendarMonthForward"
-            role="button"
+            type="button"
             onClick={nextMonth}>
             <svg viewBox="0 0 1000 1000">
               <path d="M694 242l249 250c12 11 12 21 1 32L694 773c-5 5-10 7-16 7s-11-2-16-7c-11-11-11-21 0-32l210-210H68c-13 0-23-10-23-23s10-23 23-23h806L662 275c-21-22 11-54 32-33z" />
             </svg>
-          </div>
+          </button>
           <div className="CalendarHeaderDays">
             <div className="CalendarHeaderDay">Su</div>
             <div className="CalendarHeaderDay">Mo</div>
@@ -62,7 +62,17 @@ const Calendar = (props) => {
           <tbody>
             {month.map(week => (
               <tr className="CalenderWeek">
-                {week.map(day => <td className="CalendarDay">{day}</td>)}
+                {week.map((day) => {
+                  return (
+                    <td>
+                      <input
+                        className={day ? 'CalendarDay' : 'CalendarDay Empty'}
+                        type="button"
+                        onClick={getDay}
+                        value={day} />
+                    </td>
+                  );
+                })}
               </tr>
             ))}
           </tbody>
--- client/src/reducers/calendar.js
+++ client/src/reducers/calendar.js
@@ -1,13 +1,16 @@
 const initialState = {
   addMonth: 0,
+  day: null,
 };
 
 const calendarReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'NEXT_MONTH':
-      return { ...state, countAdults: state.addMonth + 1 };
+      return { ...state, addMonth: state.addMonth + 1 };
     case 'LAST_MONTH':
-      return { ...state, countAdults: state.addMonth - 1 };
+      return { ...state, addMonth: state.addMonth - 1 };
+    case 'GET_DAY':
+      return { day: action.payload };
     default:
       return state;
   }
