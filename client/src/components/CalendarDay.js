78c0d883dff024901e42fbe8fa332821b4663046
esme <esme@github.com>
2019-07-09 17:22:04 -0700

    added state for booked calendar month

--- client/src/components/CalendarDay.js
+++ client/src/components/CalendarDay.js
@@ -1,31 +1,60 @@
+/* eslint-disable padded-blocks */
 import React from 'react';
 import moment from 'moment';
 import styles from '../../styles/calendar.module.css';
 
 const CalendarDay = (props) => {
-  const { day, getDay, checkinDay, listingInfo, momentAddedMonth, bookedDates } = props;
-  let calendarClass = 'CalendarDay Empty';
-  if (day) {
-    const formattedDay = `${momentAddedMonth.year()}-${momentAddedMonth.month() + 1}-${day}`;
-    // console.log(moment(formattedDay).format('YYYY-MM-DD'));
-    // console.log(moment(momentAddedMonth), moment());
+  const {
+    calendarRow,
+    calendarCol,
+    calendarMonth,
+    calendarDay,
+    getDay,
+    checkinDay,
+    listingInfo,
+    momentAddedMonth,
+    bookedDates
+  } = props;
+
+  // keep track of booked dates
+  const bookedCalendarMonth = [...calendarMonth];
+
+  // if calendar day does not exist, calendar day classname should be empty
+  let calendarDayClassName = 'CalendarDay Empty';
+
+  // if calendar day exists
+  if (calendarDay) {
+
+    // check booked dates which are before today, after last available date, or in booked dates obj
+    const formattedDay = `${momentAddedMonth.year()}-${momentAddedMonth.month() + 1}-${calendarDay}`;
     if (moment(formattedDay, 'YYYY-M-D').isAfter(moment(listingInfo.lastAvailableDate))
       || moment(formattedDay, 'YYYY-M-D').isBefore(moment())
       || bookedDates[moment(formattedDay, 'YYYY-M-D').format('YYYY-MM-DD')]) {
-      calendarClass = 'CalendarDay Booked';
-    } else if (day === Number(checkinDay)) {
-      calendarClass = 'CalendarDay Checkin';
+
+      calendarDayClassName = 'CalendarDay Booked';
+
+      // mark booked dates with x
+      bookedCalendarMonth[calendarRow][calendarCol] = 'x';
+
+    // if calendar day matches checkin day
+    } else if (calendarDay === Number(checkinDay)) {
+
+      // make calendar day background green
+      calendarDayClassName = 'CalendarDay Checkin';
+
     } else {
-      calendarClass = 'CalendarDay Available';
+      calendarDayClassName = 'CalendarDay Available';
     }
   }
+
   return (
     <td>
       <input
-        className={calendarClass}
+        className={calendarDayClassName}
         type="button"
-        onClick={getDay}
-        value={day} />
+        onClick={e => getDay(e, calendarRow, calendarCol, bookedCalendarMonth)}
+        value={calendarDay}
+      />
     </td>
   );
 };
--- client/src/containers/calendar.js
+++ client/src/containers/calendar.js
@@ -5,12 +5,17 @@ import { nextMonth, lastMonth, getDay } from '../actions/calendar';
 const mapStateToProps = state => ({
   addMonth: state.calendarReducer.addMonth,
   checkinDay: state.calendarReducer.checkinDay,
+  checkinDayRow: state.calendarReducer.checkinDayRow,
+  checkinDayCol: state.calendarReducer.checkinDayRow,
+  checkinCalendarMonth: state.calendarReducer.checkinCalendarMonth,
 });
 
 const mapDispatchToProps = dispatch => ({
   nextMonth: () => dispatch(nextMonth()),
   lastMonth: () => dispatch(lastMonth()),
-  getDay: e => dispatch(getDay(e)),
+  getDay: (e, calendarRow, calendarCol, bookedCalendarMonth) => (
+    dispatch(getDay(e, calendarRow, calendarCol, bookedCalendarMonth))
+  ),
 });
 
 const CalendarContainer = connect(mapStateToProps, mapDispatchToProps)(Calendar);
