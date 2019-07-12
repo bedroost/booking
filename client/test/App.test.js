63c20d29784d6beeaffb7961a0222e252999b2e4
esme <esme@github.com>
2019-07-11 17:48:33 -0700

    implement tests for toggle calendar and guests modals

--- client/test/App.test.js
+++ client/test/App.test.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-expressions */
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 
 import App from '../src/components/App';
 import BookingForm from '../src/components/BookingForm';
@@ -15,12 +17,62 @@ describe('<App />', () => {
   });
 });
 
+let isCalendarToggled;
+let isGuestsToggled;
+const onToggleCalendar = (e) => {
+  isCalendarToggled = !isCalendarToggled;
+};
+const onToggleGuests = () => {
+  isGuestsToggled = !isCalendarToggled;
+};
+const listingInfo = {
+  basePrice: 10,
+};
+
+beforeEach(() => {
+  isCalendarToggled = false;
+  isGuestsToggled = false;
+});
+
 describe('<BookingForm />', () => {
-  const wrapper = shallow(<BookingForm />);
+  const wrapper = shallow(
+    <BookingForm
+      onToggleCalendar={onToggleCalendar}
+      onToggleGuests={onToggleGuests}
+      listingInfo={listingInfo}
+    />,
+  );
+
   it('renders <GuestsContainer /> component', () => {
     expect(wrapper.find(GuestsContainer)).to.have.lengthOf(1);
   });
+
   it('renders <CalendarContainer /> component', () => {
     expect(wrapper.find(CalendarContainer)).to.have.lengthOf(1);
+  });
+
+  it('should open calendar modal on button click in checkin field', () => {
+    wrapper.find('#checkin').simulate('click');
+    expect(isCalendarToggled).to.be.true;
+  });
+
+  it('should open calendar modal on button click in checkout field', () => {
+    wrapper.find('#checkout').simulate('click');
+    expect(isCalendarToggled).to.be.true;
+  });
+
+  it('should close calendar modal on button click in booking form', () => {
+    isCalendarToggled = true;
+    wrapper.find('.BookingForm').simulate('click');
+    expect(isCalendarToggled).to.be.false;
+  });
+
+  it('should open guests modal on button click in guests field', () => {
+    wrapper.find('.Guests').simulate('click');
+    expect(isGuestsToggled).to.be.true;
+  });
+
+  it('should show price from listinginfo', () => {
+    console.log(wrapper.find('.Price').get(0).value);
   });
 });
