24a719df1b2f6a608ffaf2472ee26d0539dce312
esme <esme@github.com>
2019-07-11 22:19:41 -0700

    add test for calendar month header

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-closing-bracket-location */
 import React from 'react';
 import moment from 'moment';
 import styles from '../../styles/calendar.module.css';
@@ -59,7 +58,8 @@ const Calendar = ({
           <button
             className="CalendarMonthBackward"
             type="button"
-            onClick={lastMonth}>
+            onClick={lastMonth}
+          >
             <svg viewBox="0 0 1000 1000">
               <path d="M 336 275 L 126 485 h 806 c 13 0 23 10 23 23 s -10 23 -23 23 H 126 l 210 210 c 11 11 11 21 0 32 c -5 5 -10 7 -16 7 s -11 -2 -16 -7 L 55 524 c -11 -11 -11 -21 0 -32 l 249 -249 c 21 -22 53 10 32 32 Z" />
             </svg>
@@ -67,7 +67,8 @@ const Calendar = ({
           <button
             className="CalendarMonthForward"
             type="button"
-            onClick={nextMonth}>
+            onClick={nextMonth}
+          >
             <svg viewBox="0 0 1000 1000">
               <path d="M694 242l249 250c12 11 12 21 1 32L694 773c-5 5-10 7-16 7s-11-2-16-7c-11-11-11-21 0-32l210-210H68c-13 0-23-10-23-23s10-23 23-23h806L662 275c-21-22 11-54 32-33z" />
             </svg>
