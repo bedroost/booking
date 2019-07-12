2e57b99e52a2e32d38928de33017e7233e961455
esme <23175119+esme@users.noreply.github.com>
2019-07-12 01:52:25 -0700

    working on close form button

--- client/src/components/BookingForm.js
+++ client/src/components/BookingForm.js
@@ -73,6 +73,9 @@ class BookingForm extends React.Component {
         >
           <div className="BookingFormOutline">
             <div className="BookingFormInnerOutline">
+              {/* <button className="BookingFormCloseButton">
+                <svg viewBox="0 0 24 24" role="img" aria-label="Close" focusable="false" style={{height: '16px', width: '16px', display: 'block', fill: 'rgb(118, 118, 118)'}}><path d="m23.25 24c-.19 0-.38-.07-.53-.22l-10.72-10.72-10.72 10.72c-.29.29-.77.29-1.06 0s-.29-.77 0-1.06l10.72-10.72-10.72-10.72c-.29-.29-.29-.77 0-1.06s.77-.29 1.06 0l10.72 10.72 10.72-10.72c.29-.29.77-.29 1.06 0s .29.77 0 1.06l-10.72 10.72 10.72 10.72c.29.29.29.77 0 1.06-.15.15-.34.22-.53.22" fillRule="evenodd"></path></svg>
+              </button> */}
               <div className="BookingFormHeader">
                 <div className="BookingFormHeaderPrice">
                   <span className="Price">
